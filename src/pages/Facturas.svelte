1f43cb47f933d6f66e0294f7d7dae906f9ebe096
Miguel <tralafarlaw@gmail.com>
2021-06-05 20:34:31 -0400

    dise;o de la pagina de facturaas

--- src/pages/Facturas.svelte
+++ src/pages/Facturas.svelte
@@ -1,57 +1,219 @@
 <script>
-    import { push } from 'svelte-spa-router'
-    let search = ''
-    let facturas = []
-    let pagesize = 10
-    let datefilterStart = ''
-    let datefilterEnd = ''
-    
-    function registerFact() {
-      push('/Facturas/New')
+  import { push } from "svelte-spa-router";
+  import Datepicker from "../components/Datepicker/Datepicker.svelte";
+  let search = "";
+  let facturas = [];
+  let pagesize = 10;
+  let datefilterStart = "";
+  let datefilterEnd = "";
+  let desdeAux = new Date();
+  let hastaAux = new Date();
+
+  function registerFact() {
+    push("/Facturas/New");
+  }
+  const ondesdeChange = (d) => {
+    datefilterStart = toStr(d.detail);
+    desdeAux = d.detail;
+  };
+  const onhastaChange = (d) => {
+    datefilterEnd = toStr(d.detail);
+    hastaAux = d.detail;
+  };
+  function toStr(d) {
+    var dd = d.getDate();
+
+    var mm = today.getMonth() + 1;
+    var yyyy = today.getFullYear();
+    if (dd < 10) {
+      dd = "0" + dd;
     }
+
+    if (mm < 10) {
+      mm = "0" + mm;
+    }
+    return `${yyyy}/${mm}/${dd}`;
+  }
 </script>
 
 <div class="header-row">
-  <svg aria-hidden="true" focusable="false" data-prefix="fad" data-icon="file-invoice" role="img"
-  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"
-  class="svg-inline--fa fa-file-invoice fa-w-12 fa-5x">
- <g class="fa-group">
-     <path fill="currentColor"
-           d="M96 320h192v-64H96zm288-192H272a16 16 0 0 1-16-16V0H24A23.94 23.94 0 0 0 0 23.88V488a23.94 23.94 0 0 0 23.88 24H360a23.94 23.94 0 0 0 24-23.88V128zM64 72a8 8 0 0 1 8-8h80a8 8 0 0 1 8 8v16a8 8 0 0 1-8 8H72a8 8 0 0 1-8-8zm0 64a8 8 0 0 1 8-8h80a8 8 0 0 1 8 8v16a8 8 0 0 1-8 8H72a8 8 0 0 1-8-8zm256 304a8 8 0 0 1-8 8h-80a8 8 0 0 1-8-8v-16a8 8 0 0 1 8-8h80a8 8 0 0 1 8 8zm0-104a16 16 0 0 1-16 16H80a16 16 0 0 1-16-16v-96a16 16 0 0 1 16-16h224a16 16 0 0 1 16 16z"
-           class="fa-secondary"></path>
-     <path fill="currentColor"
-           d="M377 105L279.1 7a24 24 0 0 0-17-7H256v112a16 16 0 0 0 16 16h112v-6.1a23.9 23.9 0 0 0-7-16.9zm-73 119H80a16 16 0 0 0-16 16v96a16 16 0 0 0 16 16h224a16 16 0 0 0 16-16v-96a16 16 0 0 0-16-16zm-16 96H96v-64h192zm24 96h-80a8 8 0 0 0-8 8v16a8 8 0 0 0 8 8h80a8 8 0 0 0 8-8v-16a8 8 0 0 0-8-8z"
-           class="fa-primary"></path>
- </g>
-</svg>
-    <h1 style="margin-left: 1rem;">Facturas</h1>
-  </div>
-  <div class="controls-row">
-    <div class="input-container">
-      <svg
-        aria-hidden="true"
-        focusable="false"
-        data-prefix="fad"
-        data-icon="search"
-        role="img"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 512 512"
-        class="input-icon"
-        ><g class="fa-group"
-          ><path
-            fill="currentColor"
-            d="M208 80a128 128 0 1 1-90.51 37.49A127.15 127.15 0 0 1 208 80m0-80C93.12 0 0 93.12 0 208s93.12 208 208 208 208-93.12 208-208S322.88 0 208 0z"
-            class="fa-secondary"
-          /><path
-            fill="currentColor"
-            d="M504.9 476.7L476.6 505a23.9 23.9 0 0 1-33.9 0L343 405.3a24 24 0 0 1-7-17V372l36-36h16.3a24 24 0 0 1 17 7l99.7 99.7a24.11 24.11 0 0 1-.1 34z"
-            class="fa-primary"
-          /></g
-        ></svg
-      >
-      <!--input  bind:value={search} on:keyup={filterData} class="text-input input" placeholder="Buscar..." --> 
-      
-    </div>
+  <svg
+    aria-hidden="true"
+    focusable="false"
+    data-prefix="fad"
+    data-icon="file-invoice"
+    role="img"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 384 512"
+    class="svg-inline--fa fa-file-invoice fa-w-12 fa-5x"
   >
-    <button class="button"style="margin-left: auto;" on:click={registerFact}>Registrar Factura</button>
+    <g class="fa-group">
+      <path
+        fill="currentColor"
+        d="M96 320h192v-64H96zm288-192H272a16 16 0 0 1-16-16V0H24A23.94 23.94 0 0 0 0 23.88V488a23.94 23.94 0 0 0 23.88 24H360a23.94 23.94 0 0 0 24-23.88V128zM64 72a8 8 0 0 1 8-8h80a8 8 0 0 1 8 8v16a8 8 0 0 1-8 8H72a8 8 0 0 1-8-8zm0 64a8 8 0 0 1 8-8h80a8 8 0 0 1 8 8v16a8 8 0 0 1-8 8H72a8 8 0 0 1-8-8zm256 304a8 8 0 0 1-8 8h-80a8 8 0 0 1-8-8v-16a8 8 0 0 1 8-8h80a8 8 0 0 1 8 8zm0-104a16 16 0 0 1-16 16H80a16 16 0 0 1-16-16v-96a16 16 0 0 1 16-16h224a16 16 0 0 1 16 16z"
+        class="fa-secondary"
+      />
+      <path
+        fill="currentColor"
+        d="M377 105L279.1 7a24 24 0 0 0-17-7H256v112a16 16 0 0 0 16 16h112v-6.1a23.9 23.9 0 0 0-7-16.9zm-73 119H80a16 16 0 0 0-16 16v96a16 16 0 0 0 16 16h224a16 16 0 0 0 16-16v-96a16 16 0 0 0-16-16zm-16 96H96v-64h192zm24 96h-80a8 8 0 0 0-8 8v16a8 8 0 0 0 8 8h80a8 8 0 0 0 8-8v-16a8 8 0 0 0-8-8z"
+        class="fa-primary"
+      />
+    </g>
+  </svg>
+  <h1 style="margin-left: 1rem;">Facturas</h1>
+</div>
+<div class="controls-row">
+  <div class="input-container">
+    <svg
+      aria-hidden="true"
+      focusable="false"
+      data-prefix="fad"
+      data-icon="search"
+      role="img"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 512 512"
+      class="input-icon"
+      ><g class="fa-group"
+        ><path
+          fill="currentColor"
+          d="M208 80a128 128 0 1 1-90.51 37.49A127.15 127.15 0 0 1 208 80m0-80C93.12 0 0 93.12 0 208s93.12 208 208 208 208-93.12 208-208S322.88 0 208 0z"
+          class="fa-secondary"
+        /><path
+          fill="currentColor"
+          d="M504.9 476.7L476.6 505a23.9 23.9 0 0 1-33.9 0L343 405.3a24 24 0 0 1-7-17V372l36-36h16.3a24 24 0 0 1 17 7l99.7 99.7a24.11 24.11 0 0 1-.1 34z"
+          class="fa-primary"
+        /></g
+      ></svg
+    >
+    <!--input  bind:value={search} on:keyup={filterData} class="text-input input" placeholder="Buscar..." -->
+    <Datepicker
+      on:datechange={ondesdeChange}
+      selected={desdeAux}
+      isAllowed={(date) => {
+        const millisecs = date.getTime();
+        if (millisecs + 25 * 3600 * 1000 <= Date.now()) return true;
+        return false;
+      }}
+    />
   </div>
+  <Datepicker
+    on:datechange={ondesdeChange}
+    selected={desdeAux}
+    isAllowed={(date) => {
+      const millisecs = date.getTime();
+      if (millisecs + 25 * 3600 * 1000 <= Date.now()) return true;
+      return false;
+    }}
+  /><Datepicker
+    on:datechange={onhastaChange}
+    selected={hastaAux}
+    isAllowed={(date) => {
+      const millisecs = date.getTime();
+      if (millisecs + 25 * 3600 * 1000 <= Date.now()) return true;
+      return false;
+    }}
+  />
+  <button class="button" style="margin-left: auto;" on:click={registerFact}
+    >Registrar Factura</button
+  >
+</div>
+<div style="padding: 2rem;">
+  <div class="table-container">
+    <table class="table-body">
+      <tr>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Points</th>
+      </tr>
+      <tr>
+        <td>Jill</td>
+        <td>Smith</td>
+        <td>50</td>
+      </tr>
+      <tr>
+        <td>Eve</td>
+        <td>Jackson</td>
+        <td>94</td>
+      </tr>
+      <tr>
+        <td>Adam</td>
+        <td>Johnson</td>
+        <td>67</td>
+      </tr>
+    </table>
+    <div class="table-footer">
+      <span>## de ### </span>
+
+      <button style="margin-left: auto;" class="footer-button">
+        <svg viewBox="0 0 24 24" focusable="false" class="footer-icon"
+          ><path
+            d="M18.41 16.59L13.82 12l4.59-4.59L17 6l-6 6 6 6zM6 6h2v12H6z"
+          /></svg
+        >
+      </button>
+      <button class="footer-button">
+        <svg viewBox="0 0 24 24" focusable="false" class="mat-paginator-icon"
+          ><path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z" /></svg
+        >
+      </button>
+      <button class="footer-button">
+        <svg viewBox="0 0 24 24" focusable="false" class="mat-paginator-icon"
+          ><path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z" /></svg
+        >
+      </button>
+      <button class="footer-button">
+        <svg viewBox="0 0 24 24" focusable="false" class="mat-paginator-icon"
+          ><path
+            d="M5.59 7.41L10.18 12l-4.59 4.59L7 18l6-6-6-6zM16 6h2v12h-2z"
+          /></svg
+        >
+      </button>
+    </div>
+  </div>
+</div>
+
+<style>
+  .table-container {
+    border-radius: 1rem;
+    overflow: hidden;
+    overflow-x: auto;
+    box-shadow: 0 0 40px 0 rgba(0, 0, 0, 0.15);
+  }
+  table {
+    border-collapse: collapse;
+
+    width: 100%;
+  }
+  th {
+    font-family: Lato-Bold;
+    font-size: 18px;
+    color: #fff;
+    line-height: 1.4;
+    background-color: #6c7ae0;
+  }
+  tr:nth-child(even) {
+    background-color: #f8f6ff;
+  }
+  td {
+    padding-left: 1rem;
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+  }
+  th {
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+  }
+  .table-footer {
+    margin: 0.5rem;
+    display: inline-flex;
+    flex-wrap: wrap;
+    align-items: center;
+    padding-left: 1rem;
+  }
+  .footer-button {
+    width: 2.5rem;
+    height: 2.5rem;
+    background-color: transparent;
+    border-color: transparent;
+  }
+</style>
